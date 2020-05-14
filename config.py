ec0b22022651c9ec368668d6771ad615e571b5d0
Catherine Yang <catherineyang@u.northwestern.edu>
2020-05-14 17:29:26 -0500

    Add and update configuration-related files

---
+++ config.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import pathlib
+import datetime
+
+# Getting the parent directory of this file. That will function as the project home.
+HOME = pathlib.Path(sys.path[0])
+
+# YAML filepath
+YAML_CONFIG = HOME / "config" / "modelconfig.yml"
+
+# Date indicating version of the file to be pulled
+YEAR = 2019
+MONTH = 11
+DAY = 21
+PULL_DATE = datetime.datetime(YEAR, MONTH, DAY)
+
+# Source data URL
+URL_LISTINGS = (
+    "http://data.insideairbnb.com/united-states/il/chicago/"
+    + PULL_DATE.strftime("%Y-%m-%d")
+    + "/data/listings.csv.gz"
+)
+
+# Local data filepaths
+DATA_PATH = HOME / "data"
+DATA_FILENAME_RAW = DATA_PATH / (
+    "listings-" + PULL_DATE.strftime("%Y%m%d") + "-raw.csv"
+)
+DATA_FILENAME_CLEAN = DATA_PATH / "listings-clean.csv"
+DATA_FILENAME_NEIGHBORHOOD = DATA_PATH / "neighbourhoods.csv"
+DATA_FILENAME_FEATURES = DATA_PATH / "features.csv"
+
+# Local database filepaths
+DATABASE_PATH = HOME / "data" / "airbnbchi.db"
--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -1,8 +1,9 @@
+# TODO: To be updated
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
 PORT = 5000
 APP_NAME = "penny-lane"
-SQLALCHEMY_DATABASE_URI = 'sqlite:///data/tracks.db'
+SQLALCHEMY_DATABASE_URI = "sqlite:///data/tracks.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 HOST = "0.0.0.0"
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
